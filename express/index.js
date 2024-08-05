b0b68a61e81d7639c8ae196e3cadce6d7abd508c
khanhngan1491 <khanhngan1491@gmail.com>
2024-08-05 17:35:49 +0000

    2

--- express/index.js
+++ express/index.js
@@ -1,16 +1,17 @@
+import express, { json } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { connect } from "mongoose";
+import cookieParser from "cookie-parser";
 
-
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-
+config();
 const app = express();
+
+connect(process.env.MONGODB_URL)
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(json());
 
 app.listen(8000, () => {
     console.log("Server is running");
